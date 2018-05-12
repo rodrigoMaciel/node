16cdb1d811a63336ff5e1857e1f429235aa54b65
Rodrigo Freitas <macielfreitas@hotmail.com.br>
2018-05-12 14:26:07 -0300

    Adcionado projeto em node

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var server = http.createServer(function(request,response)
+{
+    response.writeHead(
+        200,
+        {"Content-Type":"text/html"}
+    );
+
+    if(request.url === '/') {
+        response.write('<h1>Meu app JS</h1>');
+    } else if (request.url === '/pos') {
+        response.write('<h1>SENAC</h1>');
+    } else {
+        response.write('<h1>Essa rota existe?</h1>');
+    }
+    response.end();
+});
